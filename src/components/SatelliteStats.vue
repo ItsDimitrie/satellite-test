8cc1a15278df6fb4e794843c2b28b5bc503d2c53
ItsDimitrie <laue0002@HZ.nl>
2024-12-09 15:40:00 +0800

    Added basic components

---
+++ src/components/SatelliteStats.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="stats">
+    <h2>Satellite Stats</h2>
+    <ul>
+      <li>Altitude: {{ stats.altitude }} km</li>
+      <li>Velocity: {{ stats.velocity }} km/s</li>
+      <li>Status: {{ stats.status }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['stats'],
+};
+</script>
+
+<style>
+.stats { /* Add styling */ }
+</style>
+
---
+++ src/components/TelemetryLogs.vue
@@ -0,0 +1,29 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Time</th>
+        <th>Parameter</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="log in logs" :key="log.id">
+        <td>{{ log.time }}</td>
+        <td>{{ log.parameter }}</td>
+        <td>{{ log.value }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: ['logs'],
+};
+</script>
+
+<style>
+table { /* Add styling */ }
+</style>
+
